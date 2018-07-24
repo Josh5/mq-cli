2c197d0802597e767a599ac63a6d33c5b23e76d1
Josh.5 <jsunnex@gmail.com>
2018-07-25 10:17:13 +1200

    Update mantis extension to include support for just adding a cookie string to config rather than user/pass Tidy up code

--- extensions/mantis.py
+++ extensions/mantis.py
@@ -94,38 +94,39 @@ class REMOTE():
 class Mantis(object):
 
     def __init__(self, *args, **kwargs):
-        self.config     = Config()
-        self.config.read_config()
+        self.config     = Config();
+        self.config.read_config();
         if 'debug' in kwargs:
-            self.debug  = kwargs.get("debug")
-        self.USERNAME   = self.getConfig("mantis_username")
-        self.PASSWORD   = self.getConfig("mantis_password")
-        self.LOGIN_URL  = self.getConfig("mantis_login_url")
-        self.BASE_URL   = self.getConfig("mantis_url")
-        self.req        = REMOTE(debug=self.debug)
-        self.headers    = [];
+            self.debug  = kwargs.get("debug");
+        self.USERNAME   = self.getConfig("mantis_username");
+        self.PASSWORD   = self.getConfig("mantis_password");
+        self.LOGIN_URL  = self.getConfig("mantis_login_url");
+        self.COOKIE     = self.getConfig("mantis_cookie");
+        self.BASE_URL   = self.getConfig("mantis_url");
+        self.req        = REMOTE(debug=self.debug);
+        self.headers    = {"Cookie": self.COOKIE};
 
     def getConfig(self, config_id):
         if str(config_id) in self.config.getAll():
-            return getattr(self.config, config_id)
-        return ""
+            return getattr(self.config, config_id);
+        return "";
 
     def log(self, string):
         if self.debug:
             try:
-                print('[MANTIS]: %s' % string)
+                print('[MANTIS]: %s' % string);
             except UnicodeEncodeError:
                 # we can't anticipate everything in unicode they might throw at
                 # us, but we can handle a simple BOM
-                bom = unicode(codecs.BOM_UTF8, 'utf8')
-                print('[MANTIS]: %s' % string.replace(bom, ''))
+                bom = unicode(codecs.BOM_UTF8, 'utf8');
+                print('[MANTIS]: %s' % string.replace(bom, ''));
             except Exception:
                 pass
 
     def login(self, redirect_issue=""):
-        _url = "https://www.nsquared.co.nz/support/tickets/login.php"
+        _url = self.LOGIN_URL;
         if not self.PASSWORD:
-            self.PASSWORD = getpass.getpass("Enter the Password:")
+            self.PASSWORD = getpass.getpass("Enter the Password:");
         data = {
             "username": self.USERNAME,
             "password": self.PASSWORD,
@@ -134,97 +135,99 @@ class Mantis(object):
         }
         ret = self.req.make_request(
             _url, 'post', payload=data, allow_redirects=False)
-        self.log(ret.content)
+        self.log(ret.content);
         if ret.status_code == 302:
-            self.headers = ret.headers
-            self.log("successfully logged in")
-            return True
-        return False
+            self.COOKIE = ret.headers['Set-Cookie'];
+            self.headers['Cookie'] = self.COOKIE;
+            self.log("successfully logged in");
+            return True;
+        return False;
 
     def list_patches_on_mantis_bug(self, issue):
-        _url = self.BASE_URL + "/view.php?id={}".format(issue)
-        self.login()
-        ret = self.req.make_request(_url, 'get')
+        _url = self.BASE_URL + "/view.php?id={}".format(issue);
+        if not self.COOKIE:
+            self.login();
+        ret = self.req.make_request(_url, 'get', headers=self.headers);
         self.log(ret.content)
         tree = html.fromstring(ret.content)
-        notes = tree.xpath('//*[contains(@class, "bugnote-note")]//a[text()]')
-        issue_patches = {}
+        notes = tree.xpath('//*[contains(@class, "bugnote-note")]//a[text()]');
+        issue_patches = {};
         for note in notes:
-            text = note.xpath('text()')
+            text = note.xpath('text()');
             if isinstance(text, list):
-                text = text[0]
+                text = text[0];
             if 'issue' in text.lower():
-                link = note.xpath('@href')[0]
+                link = note.xpath('@href')[0];
                 if isinstance(link, list):
-                    link = link[0]
-                issue_patches[text] = link
-        return issue_patches
+                    link = link[0];
+                issue_patches[text] = link;
+        return issue_patches;
 
     def download_file(self, url, filename):
-        _file   = os.path.join('/', 'tmp', filename)
-        _url    = self.BASE_URL + "/{}".format(url)
-        ret     = self.req.make_request(_url, 'getfile')
+        _file   = os.path.join('/', 'tmp', filename);
+        _url    = self.BASE_URL + "/{}".format(url);
+        ret     = self.req.make_request(_url, 'getfile', headers=self.headers);
         if ret.status_code == 200:
             with open(_file, 'wb') as f:
                 for chunk in ret.iter_content(chunk_size=1024): 
                     if chunk:
-                        f.write(chunk)
-            return _file
-        return False
+                        f.write(chunk);
+            return _file;
+        return False;
 
     def import_patch_from_mantis(self, issue=""):
-        print("")
-        print("Importing patch from mantis...")
-        print("")
+        print("");
+        print("Importing patch from mantis...");
+        print("");
         while not issue:
-            issue = input('Enter an issue number: ')
+            issue = input('Enter an issue number: ');
         patch_list = self.list_patches_on_mantis_bug(issue);
-        count = 1
-        selection_list = {}
-        print("")
+        count = 1;
+        selection_list = {};
+        print("");
         for key in patch_list:
-            print("      {}) {}".format(count, key))
-            selection_list[str(count)] = patch_list[key]
-            count += 1
-        print("")
-        selection = input("Select a patch from the list above: ")
+            print("      {}) {}".format(count, key));
+            selection_list[str(count)] = patch_list[key];
+            count += 1;
+        print("");
+        try:
+            selection = input("Select a patch from the list above: ");
+        except:
+            selection = False;
         if selection and selection in selection_list:
             selected_url = selection_list[selection]
-            res_file = self.download_file(selected_url, "ISSUE_{}.patch".format(issue))
+            res_file = self.download_file(selected_url, "ISSUE_{}.patch".format(issue));
             if res_file:
-                print("")
-                print("")
-                cmd = ['mq', 'import', res_file, '--name={}'.format(issue)]
-                p = subprocess.check_call(cmd)
-                if p == 0:
-                    return True
+                print("");
+                print("");
+                cmd = ['mq', 'import', res_file, '--name={}'.format(issue)];
+                try:
+                    p = subprocess.check_call(cmd);
+                    if p == 0:
+                        return True;
+                except:
+                    pass;
         else:
-            print("")
-            print("That was not a valid selection.")
-        print("No patch imported.")
-        return False
+            print("");
+            print("That was not a valid selection.");
+        print("No patch imported.");
+        return False;
 
     def upload(self, file, issue):
-        global req
         issue_url = self.BASE_URL + '/view.php?id={}'.format(issue)
-        ret = req.make_request(issue_url, 'get', allow_redirects=True)
+        ret = self.req.make_request(issue_url, 'get', allow_redirects=True, headers=self.headers)
         self.log(ret.headers)
         self.log(ret.cookies)
         tree = html.fromstring(ret.content)
-        bugnote_add_token = tree.xpath(
-            '//input[@name="bugnote_add_token"]/@value')
+        bugnote_add_token = tree.xpath('//input[@name="bugnote_add_token"]/@value')
         bug_id = tree.xpath('//input[@name="bug_id"]/@value')
         bugnote_text = tree.xpath('//input[@name="bugnote_text"]/@value')
         max_file_size = tree.xpath('//input[@name="max_file_size"]/@value')
-        #bugnote_add_token = ""
         if os.path.exists(file):
             self.log("uploading file " + file)
-            #files = {'ISSUE_12440_gocabinets.hg.patch': open(file, 'rb')}
-            #files = {"file": ("ISSUE_12440_gocabinets.hg.patch", open(filepath, "rb"))}
             headers = {
                 "Accept": "application/json",
-                "Origin": "https://www.nsquared.co.nz",
-                "Host": "www.nsquared.co.nz",
+                "Cookie": self.COOKIE,
                 "Referer": issue_url,
                 "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36",
                 "X-Requested-With": "XMLHttpRequest",
